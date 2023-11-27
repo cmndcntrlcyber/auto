a40485897a6b3b51a4e8bedc5a214dfc7d5fed5a
cmndcntrlcyber <r.soreng@c3s.consulting>
2023-11-27 12:35:16 -0800

    modified:   build-flask.sh

--- build-flask.sh
+++ build-flask.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-wget "https://raw.githubusercontent.com/cmndcntrlcyber/auto/main/fresh-ubun.sh" | bash -
+wget "https://raw.githubusercontent.com/cmndcntrlcyber/auto/main/fresh-ubun.sh" 
+bash fresh-ubun.sh
 
 echo "What is the name of your app?"
 read name
 
-curl "https://raw.githubusercontent.com/cmndcntrlcyber/auto/main/start-apache2.sh" | bash
+wget "https://raw.githubusercontent.com/cmndcntrlcyber/auto/main/start-apache2.sh" 
+bash start-apache2.sh
 
 cd /var/www/; mkdir apps; cd apps
 git clone "https://github.com/cmndcntrl/ztsc.git"
@@ -14,7 +16,7 @@ cd $name;
 
 sed -i 's/ztsc/$name/g' 000-default.conf
 sudo a2ensite 000-default.conf
-python -m venv $name;  source $name/bin/activate # $name here begins representing the virutal environment
 curl "https://raw.githubusercontent.com/cmndcntrlcyber/auto/main/prep-flask-web.py" | python3
+virutalenv -p python3 $name # $name here begins representing the virutal environment
 source $name/bin/activate
-export FLASK_APP=$name.py
+export FLASK_APP=$name.py # $name is the flask app name in this line
