95fc1c582581b02d145ef15d7a52a05c5762987c
cmndcntrlcyber <r.soreng@c3s.consulting>
2024-04-30 10:30:58 -0500

    modified files

---
+++ scripts/engagers/ransim.py
@@ -0,0 +1,6 @@
+# A ransomware simulator
+# For educational and professional use only. Do not used for 
+# aunauthorized or illegal activities. The creator of this 
+# project does not condone nor is responsible for any illegal 
+# or unauthorized uses of this script.
+
