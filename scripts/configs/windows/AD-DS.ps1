a7615b6e546b522ff361de480b9127177b09260a
cmndcntrlcyber <r.soreng@c3s.consulting>
2024-05-16 10:41:13 -0500

    created file

---
+++ scripts/configs/windows/AD-DS.ps1
@@ -0,0 +1,17 @@
+#
+# Windows PowerShell script for AD DS Deployment
+#
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "d3fend-node.net" `
+-DomainNetbiosName "D3FEND-NODE" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
